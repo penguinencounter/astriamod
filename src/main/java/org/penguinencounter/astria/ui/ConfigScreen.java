10818d9d4a90f50af46384f248abf311cc5f3ae5
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-10-26 09:11:20 -0700

    startUI

--- src/main/java/org/penguinencounter/astria/ui/ConfigScreen.java
+++ src/main/java/org/penguinencounter/astria/ui/ConfigScreen.java
@@ -1,7 +1,6 @@
 package org.penguinencounter.astria.ui;
 
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 
@@ -13,21 +12,39 @@ public class ConfigScreen extends Screen {
         super(Text.translatable("astria.config"));
     }
 
+    private int darkenColor(int in) {
+        int out = 0;
+        for (int i = 0; i < 3; i++) {
+            int c = (in >> (i * 8)) & 0xFF;
+            c = (int) (c * 0.5);
+            out |= c << (i * 8);
+        }
+        int a = (in >> 24) & 0xFF;
+        out |= a << 24;
+        return out;
+    }
+
+    private void drawVerticalLineWithLayer(MatrixStack matrices, int x, int y1, int y2, int color) {
+        drawVerticalLine(matrices, x+2, y1+2, y2+2, darkenColor(color));
+        drawVerticalLine(matrices, x, y1, y2, color);
+    }
+
+    private void drawHorizontalLineWithLayer(MatrixStack matrices, int x1, int x2, int y, int color) {
+        drawHorizontalLine(matrices, x1+2, x2+2, y+2, darkenColor(color));
+        drawHorizontalLine(matrices, x1, x2, y, color);
+    }
+
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         super.render(matrices, mouseX, mouseY, delta);
+        renderBackground(matrices);
+        drawVerticalLineWithLayer(matrices, width/4, height/4, height/4*3, 0xffffffff);
+        drawVerticalLineWithLayer(matrices, width/4*3, height/4, height/4*3, 0xffffffff);
+        drawHorizontalLineWithLayer(matrices, width/4, width/4*3, height/4, 0xffffffff);
+        drawHorizontalLineWithLayer(matrices, width/4, width/4*3, height/4*3, 0xffffffff);
     }
 
     @Override
     protected void init() {
-        this.addDrawableChild(
-                new ButtonWidget(this.width / 2 - 100, this.height / 2 - 10, 200, 20,
-                        Text.literal("what"),
-                        button -> {
-                            if (this.client == null || this.client.player == null) return;
-                            this.client.player.sendCommand("me HELP", null);
-                        }
-                )
-        );
     }
 }
